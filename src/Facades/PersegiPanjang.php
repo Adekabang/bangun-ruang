1da678ff14069a46773d6c1dbacbcd9dde744f3d
Mohammad Raska <iban.ninja@gmail.com>
2020-03-29 10:26:59 +0700

    initial package

---
+++ src/Facades/PersegiPanjang.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace adekabang\BangunRuang\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class PersegiPanjang extends Facade 
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'persegi-panjang';
+    }
+}
---
+++ src/Helpers/PersegiPanjang.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace adekabang\BangunRuang\Helpers;
+
+class PersegiPanjang{
+    public function hitungLuas($panjang, $lebar){
+        return $panjang * $lebar;
+    }
+}
